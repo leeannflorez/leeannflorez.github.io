cba3050684a39d5464079adb35cc893b5c8def16
LeeAnn Florez <lflorez01@wildcats.jwu.edu>
2018-11-05 22:34:36 -0500

    first draft for critique

--- js/main.js
+++ js/main.js
@@ -3,14 +3,10 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-    document.getElementById("nav").style.padding = "10px";
-    document.getElementById("logo").style.width = "5%";
     document.getElementById("title").style.display = "none";
-    document.getElementById("navul").style.padding = "20px 0px 0px 0px";
+    document.getElementById("nav").style.margin = "0px";
+
   } else {
-    document.getElementById("nav").style.padding = "10px 10px";
-    document.getElementById("logo").style.width = "10%";
     document.getElementById("title").style.display = "block";
-    document.getElementById("navul").style.padding = "0px";
   }
 }
